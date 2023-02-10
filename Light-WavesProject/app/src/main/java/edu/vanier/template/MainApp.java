c695aaa643340be849ced04fb66ccffaed0bb0c7
Matthew <114512014+xif19@users.noreply.github.com>
2023-02-10 08:55:49 -0500

    First Project Template
    first project

---
+++ Light-WavesProject/app/src/main/java/edu/vanier/template/MainApp.java
@@ -0,0 +1,36 @@
+package edu.vanier.template;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+/**
+ * This is a JavaFX project template to be used for creating GUI applications.
+ * JavaFX 18 is already linked to this project in the build.gradle file.
+ * @link: https://openjfx.io/javadoc/18/
+ * @see: Build Scripts/build.gradle
+ * @author Sleiman Rabah.
+ */
+public class MainApp extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        //asdasd
+        VBox root = new VBox();
+        
+        Scene scene = new Scene(root, 300, 300);
+        
+        stage.setScene(scene);        
+
+        stage.setTitle("This is a JavaFX app template...");
+        
+        stage.sizeToScene();
+        
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
