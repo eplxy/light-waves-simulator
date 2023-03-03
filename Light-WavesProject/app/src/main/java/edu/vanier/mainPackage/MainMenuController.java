434457f3309f903bdf3c0da3f79cb0b0c8db630d
Matthew <>
2023-03-03 09:20:21 -0500

    Added names to fxml

---
+++ Light-WavesProject/app/src/main/java/edu/vanier/mainPackage/MainMenuController.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.vanier.mainPackage;
+
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author 2148289
+ */
+public class MainMenuController {
+    
+    Stage primaryStage; 
+    
+    public MainMenuController(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+    }
+    
+    public void initialize(){
+        
+    }
+}
