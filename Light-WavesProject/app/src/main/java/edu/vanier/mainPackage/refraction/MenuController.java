0aa93fd2b081ce6b36fc7341bde3c544a8aa5e98
Matthew <>
2023-03-06 09:48:04 -0500

    Scene builder done

---
+++ Light-WavesProject/app/src/main/java/edu/vanier/mainPackage/refraction/MenuController.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.vanier.mainPackage.refraction;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Slider;
+
+/**
+ *
+ * @author 2148289
+ */
+public class MenuController {
+    
+    @FXML
+    Button btnMainMenu;
+    @FXML
+    Button btnLight;
+    @FXML
+    Button btnAngles;
+    @FXML
+    Slider sliderColor;
+    @FXML
+    ChoiceBox btnMaterial1;
+    @FXML
+    ChoiceBox btnMaterial2;
+    
+    public void initialize(){
+        System.out.println("yes");
+    }
+
+    
+    
+}
--- Light-WavesProject/app/src/main/java/edu/vanier/mainPackage/refraction/Refraction.java
+++ Light-WavesProject/app/src/main/java/edu/vanier/mainPackage/refraction/Refraction.java
@@ -1,5 +1,13 @@
 package edu.vanier.mainPackage.refraction;
 
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -9,6 +17,23 @@ package edu.vanier.mainPackage.refraction;
  *
  * @author 2148289
  */
-public class Refraction {
+public class Refraction extends Application{
     
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/refractionMenu.fxml"));
+        loader.setController(new MenuController());
+        BorderPane root = loader.load();
+        Scene scene = new Scene(root, 1000, 600);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+        
+    }
+    
+    public static void main(String[] args) {
+        System.out.println("yes");
+        launch(args);
+    }
+
 }
