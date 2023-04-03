cd89d5ed032235344f195cec7009654a77111a8f
2143806 <2143806@d24203-24cxth3.VanierCollege.intra>
2023-04-03 08:27:18 -0400

    adding driver/deleting animation

--- Light-WavesProject/app/src/main/java/edu/vanier/mainPackage/photoelectriceffect/ui/PhotoelectricMain.java
+++ Light-WavesProject/app/src/main/java/edu/vanier/mainPackage/photoelectriceffect/ui/PhotoelectricMain.java
@@ -13,22 +13,6 @@ import javafx.util.Duration;
 
 //@author Maesha Mahmud
 public class PhotoelectricMain extends Application {
-
-    /*
-    @Override
-    public void start(Stage primaryStage) {
-        Pane pane = new Pane();
-
-        Electrons electrons = new Electrons(50, 50, 10);
-
-        pane.getChildren().add(electrons.getCircle());
-
-
-        Scene scene = new Scene(pane, 300, 100);
-        primaryStage.setScene(scene);
-        primaryStage.show();
-    }*/
-    //main
     @Override
     public void start(Stage stage) throws IOException {
         try {
@@ -48,11 +32,6 @@ public class PhotoelectricMain extends Application {
             stage.setScene(scene);
             stage.sizeToScene();
             stage.show();
-
-            //photoelectron animation
-            Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(0.017), event -> controller.moveCircle()));
-            timeline.setCycleCount(Timeline.INDEFINITE);
-            timeline.play();
 
         } catch (Exception e) {
             System.out.println("there was an exception in the program: " + e + " and " + e.getCause());
