584935d639c7f3b057ca04010411523d138e7d6c
Matthew <>
2023-04-30 11:34:48 -0400

    added menu button, added slider, fixed some bugs

--- Light-WavesProject/app/src/main/java/edu/vanier/mainPackage/refraction/Refraction.java
+++ Light-WavesProject/app/src/main/java/edu/vanier/mainPackage/refraction/Refraction.java
@@ -24,14 +24,12 @@ public class Refraction extends Application{
     @Override
     public void start(Stage primaryStage) throws Exception {
         
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/refractionMenu23.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/refractionMenu26.fxml"));
         MenuController menuController = new MenuController(primaryStage);
         loader.setController(menuController);
         BorderPane root = loader.load();
         
-         
         Scene scene = new Scene(root);
-        primaryStage.setResizable(false);
         primaryStage.setScene(scene);
         primaryStage.show();
         
