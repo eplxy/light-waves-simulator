b8f78861da842b3ff216a78e94befca8e70ef528
Steven <2186084@edu.vaniercollege.qc.ca>
2023-05-14 22:50:27 -0400

    Product Version - Final Commit

---
+++ Light-WavesProject/app/src/main/java/edu/vanier/mainPackage/BridgeMain.java
@@ -0,0 +1,12 @@
+package edu.vanier.mainPackage;
+
+/**
+ *
+ * @author Steven
+ */
+public class BridgeMain {
+    
+    public static void main(String[] args) {
+        MainApp.main(args);
+    }
+}
